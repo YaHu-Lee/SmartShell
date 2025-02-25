f0e07c932e948b86bcb9548875937cf00e6c32ae
Rabbit <928815137@qq.com>
2025-02-26 02:16:53 +0800

    feat: update prompts and base model

--- src/baseModel.ts
+++ src/baseModel.ts
@@ -1,8 +1,19 @@
 import { ChatOllama } from "@langchain/ollama";
 import { OllamaEmbeddings } from "@langchain/ollama";
+import { ChatOpenAI } from "@langchain/openai";
+import { llmKey, llmBaseURL, llmModel } from "../.llm_key.json"; // 离线模型参数。这个文件中包含 api key 所以会添加到 .gitignore 中
 
-export const model = new ChatOllama({
-  model: "llama3-groq-tool-use:8b", // Default value.
+export const offlineModel = new ChatOllama({
+  model: "qwen2.5:14b", // Default value.
+  verbose: true,
+});
+
+export const onlineModel = new ChatOpenAI({
+  model: llmModel,
+  configuration: {
+    baseURL: llmBaseURL,
+    apiKey: llmKey,
+  },
   temperature: 0,
   verbose: true,
 });
